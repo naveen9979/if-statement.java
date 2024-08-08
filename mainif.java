189306e86644bf86e4020e8dc3b19bf4bf7362c3
naveen9979 <sathyapriyak2020@gmail.com>
2024-08-08 10:38:19 -0700

    Add files via upload

---
+++ mainif.java
@@ -0,0 +1,26 @@
+public class mainif {
+    public static void main(String[]args) {
+        int x = 23;
+        int y = 20;
+        if (x > y) {
+            System.out.println("x is greater than y");
+        }
+
+
+        int day = 20;
+        if (day < 18) {
+            System.out.println("good evening");
+        } else {
+            System.out.println("today is wonderful day");
+        }
+
+       int number = 100; 
+       if (number < 50) {
+        System.out.println("the number is less than 50");
+     } else if (number < 200); {
+        System.out.println("the number is between 80 and 90.");
+     }
+
+    }
+    
+}
